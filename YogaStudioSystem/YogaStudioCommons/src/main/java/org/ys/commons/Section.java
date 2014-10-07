63c998ae4dbe51645e153524314e4f22e4da3137
MdmRahman <MdmRahman@CS_LAB113-203.cs.mum.edu>
2014-10-07 14:36:13 -0500

    Courses Part

--- YogaStudioSystem/YogaStudioCommons/src/main/java/org/ys/commons/Section.java
+++ YogaStudioSystem/YogaStudioCommons/src/main/java/org/ys/commons/Section.java
@@ -1,5 +1,36 @@
 package org.ys.commons;
 
-public class Section {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+@Entity
+public class Section {
+	@Id @GeneratedValue
+	private long Id;
+	
+	private String sectionName;
+
+	public Section(long id, String sectionName) {
+		this.Id = id;
+		this.sectionName = sectionName;
+	}
+
+	public long getId() {
+		return Id;
+	}
+
+	public void setId(long id) {
+		Id = id;
+	}
+
+	public String getSectionName() {
+		return sectionName;
+	}
+
+	public void setSectionName(String sectionName) {
+		this.sectionName = sectionName;
+	}
+	
+	
 }
