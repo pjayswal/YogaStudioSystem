fe934ee9bbda3daba5f1df4fa97e70a0298e92d8
MdmRahman <MdmRahman@CS_LAB113-203.cs.mum.edu>
2014-10-07 14:33:57 -0500

    Courses Part

--- YogaStudioSystem/YogaStudioCommons/src/main/java/org/ys/commons/Semester.java
+++ YogaStudioSystem/YogaStudioCommons/src/main/java/org/ys/commons/Semester.java
@@ -1,5 +1,36 @@
 package org.ys.commons;
 
-public class Semester {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+@Entity
+public class Semester {
+	@Id @GeneratedValue
+	private long Id;
+	
+	private String semesterName;
+
+	public Semester(long id, String semesterName) {
+		this.Id = id;
+		this.semesterName = semesterName;
+	}
+
+	public long getId() {
+		return Id;
+	}
+
+	public void setId(long id) {
+		Id = id;
+	}
+
+	public String getSemesterName() {
+		return semesterName;
+	}
+
+	public void setSemesterName(String semesterName) {
+		this.semesterName = semesterName;
+	}
+	
+	
 }
