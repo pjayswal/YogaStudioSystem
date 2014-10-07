a243cdaa445f76adcd2983265af3c70d75fff6de
ASodnomdorj <ASodnomdorj@CS_LAB113-205.cs.mum.edu>
2014-10-07 14:29:37 -0500

    ECommerce part updated

--- YogaStudioSystem/YogaStudioCommons/src/main/java/org/ys/commons/Product.java
+++ YogaStudioSystem/YogaStudioCommons/src/main/java/org/ys/commons/Product.java
@@ -7,10 +7,56 @@ import javax.persistence.Id;
 @Entity
 public class Product {
 	@Id @GeneratedValue
-	private long Id;
+	private long id;
 	
-	private int catID;
+	private long catergoryid;
 	private String name;
+	private double price;
 	
 
+	public Product() {
+		
+	}
+	
+	public Product(long id, long catergoryid, String name, double price) {
+		super();
+		this.id = id;
+		this.catergoryid = catergoryid;
+		this.name = name;
+		this.price = price;
+	}
+
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public long getCatergoryid() {
+		return catergoryid;
+	}
+	
+	public void setCatergoryid(long catergoryid) {
+		this.catergoryid = catergoryid;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
 }
