57a25d59a06d9e2c8d06b32e4bbfb80620d50014
NomanMannan <nomanbinmannan@gmail.com>
2014-10-10 21:30:07 -0600

    Customer controller, registration form is added

---
+++ YogaStudioWebPortal/src/main/java/org/ys/controllers/CustomerController.java
@@ -0,0 +1,35 @@
+package org.ys.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.ys.commons.Customer;
+import org.ys.commons.Person;
+
+@Controller
+public class CustomerController {
+
+	@RequestMapping(value = "/registration.html", method = RequestMethod.GET)
+	public ModelAndView getRegistrationForm() {
+
+		ModelAndView model = new ModelAndView("RegistrationForm");
+
+		return model;
+	}
+
+	@RequestMapping(value = "/submitRegistrationForm.html", method = RequestMethod.POST)
+	public ModelAndView submitRegistrationForm(@ModelAttribute("customer1") Customer customer, BindingResult result) {
+		if (result.hasErrors()) {
+			ModelAndView model = new ModelAndView("RegistrationForm");
+			return model;
+		}
+
+		ModelAndView model = new ModelAndView("RegistrationSuccess");
+		model.addObject("welcomeMessage", "Congratulations!! You are successfully registered to Yoga Studio");
+
+		return model;
+	}
+}
---
+++ YogaStudioWebPortal/src/main/webapp/WEB-INF/views/RegistrationForm.jsp
@@ -0,0 +1,40 @@
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Registration Form</title>
+</head>
+<body>
+
+	<h1 align="center">
+		<b>REGISTRATION FORM</b>
+	</h1>
+	<form:errors path="customer1.*"/>
+	
+	<form action="/YogaStudioWebPortal/submitRegistrationForm.html"
+		method="post">
+		<p>
+			Customer's Name : <input type="text" name="name" />
+		</p>
+		<p>
+			Email : <input type="text" name="email" />
+		</p>
+		<p>
+			Phone : <input type="text" name="phone" />
+		</p>
+		<p>
+			Date of Birth : <input type="text" name="dob" />
+		</p>
+		<p>
+			User Name : <input type="text" name="user.username" />
+		</p>
+		<p>
+			Password : <input type="password" name="user.password" />
+		</p>
+
+		<input type="submit" value="Register" />
+	</form>
+</body>
+</html>
+</body>
+</html>
