0be698b09c0316be848dd348d96d6595808d6683
pjayswal <free8ird@gmail.com>
2014-10-10 21:29:19 -0500

    Basic Test on CustomerDAO done

--- YogaStudioSystem/YogaStudioDAO/src/test/java/org/ys/dao/CustomerDAOTest.java
+++ YogaStudioSystem/YogaStudioDAO/src/test/java/org/ys/dao/CustomerDAOTest.java
@@ -1,5 +1,7 @@
 package org.ys.dao;
 
+import java.util.Date;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,33 +10,40 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 
+import org.ys.commons.Customer;
+import org.ys.commons.Role;
+import org.ys.commons.UserCredential;
+
 import junit.framework.TestCase;
 
 @ContextConfiguration(locations = {"classpath:applicationContext-test.xml"})
 @RunWith(SpringJUnit4ClassRunner.class)
 @Transactional
 public class CustomerDAOTest extends TestCase {
-	/*@Autowired
-	private CustomerDAO customerDAO;*/
+	@Autowired
+	private CustomerDAO customerDAO;
 	
 	@Test
     public void testContext() {
-        assertNotNull("asd");
+        assertNotNull(customerDAO);
     }
 	
 	/**
 	 * Test of create method of CustomerDAO
 	 */
-	/*@Test
+	@Test
 	public void testCreate() {
 		System.out.println("Create Customer");
+		
 		UserCredential upramod = new UserCredential("pramod", "jayswal");
-		Role roleCustomer = new Role(Role.ROLE_CUSTOMER);
+		Role roleCustomer = new Role("ROLE_CUSTOMER");
 		upramod.addRole(roleCustomer);
-		Customer customer = new Customer("Pramod", "prmaod@gmail.com", "988-21312321", DateUtil.convert("1234/12/12"), upramod);
+		//Date date = new Date();
+		Customer customer = new Customer("Pramod", "prmaod@gmail.com", "988-21312321", new Date(), upramod);
 		customerDAO.create(customer);
+		
 
-	}*/
+	}
 	
 
 }
