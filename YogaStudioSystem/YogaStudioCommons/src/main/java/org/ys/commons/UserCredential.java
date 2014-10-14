c65bdd02fb7b20e725dfca23efaa22466371a776
pjayswal <free8ird@gmail.com>
2014-10-14 16:07:08 -0500

    removed cascading for Roles.

--- YogaStudioSystem/YogaStudioCommons/src/main/java/org/ys/commons/UserCredential.java
+++ YogaStudioSystem/YogaStudioCommons/src/main/java/org/ys/commons/UserCredential.java
@@ -3,11 +3,10 @@ package org.ys.commons;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.JoinTable;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 @Entity
 @Table(name="user")
@@ -19,7 +18,7 @@ public class UserCredential {
 	private String password;
 	private boolean enabled=false;
 	
-	@OneToMany(cascade=CascadeType.ALL)
+	@ManyToMany
 	@JoinTable(name="user_role")
 	private List<Role> roles= new ArrayList<Role>();
 	
