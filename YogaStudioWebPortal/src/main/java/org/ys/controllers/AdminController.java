7ff3cf08f7a6bd519e7e0cb8a7fb69cb4cf97351
pjayswal <free8ird@gmail.com>
2014-10-14 22:01:15 -0500

    To test pull for forked repository

--- YogaStudioWebPortal/src/main/java/org/ys/controllers/AdminController.java
+++ YogaStudioWebPortal/src/main/java/org/ys/controllers/AdminController.java
@@ -1,17 +1,25 @@
 package org.ys.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.ys.clientservices.IAdminService;
 import org.ys.commons.Course;
 import org.ys.commons.Faculty;
 import org.ys.commons.Semester;
 
 @Controller
+@RequestMapping(value="/admin")
 public class AdminController {
+	@Autowired
+	private IAdminService adminService;
 
 	@RequestMapping(value = "/addcourse", method = RequestMethod.GET)
 	public ModelAndView getCourseForm() {
@@ -20,6 +28,14 @@ public class AdminController {
 
 		return model;
 	}
+	
+	@RequestMapping(value = "/semester", method = RequestMethod.GET)
+	public String viewSemesters(Model model) {
+		List<Semester> semesters = adminService.getSemesters(); 
+		model.addAttribute("semesters",semesters);
+		return "listsemesters";
+	}
+	
 
 	@RequestMapping(value = "/addCourseSubmit", method = RequestMethod.POST)
 	public ModelAndView submitCourseForm(
--- YogaStudioWebPortal/src/main/java/org/ys/controllers/CustomerController.java
+++ YogaStudioWebPortal/src/main/java/org/ys/controllers/CustomerController.java
@@ -1,6 +1,7 @@
 package org.ys.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,10 @@ public class CustomerController {
 		ModelAndView model = new ModelAndView("RegistrationForm");
 
 		return model;
+	}
+	
+	public String getSemester(Model model){
+		return null;
 	}
 
 	@RequestMapping(value = "/submit", method = RequestMethod.POST)
