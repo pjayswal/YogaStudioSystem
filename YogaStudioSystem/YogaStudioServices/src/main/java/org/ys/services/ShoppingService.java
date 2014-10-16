49cde9a11fc55c5a06ea5a829553f3f3014c88e3
AmarboldS <s_amarbold@yahoo.com>
2014-10-16 16:58:20 -0500

    Shopping service implemented

---
+++ YogaStudioSystem/YogaStudioServices/src/main/java/org/ys/services/ShoppingService.java
@@ -0,0 +1,101 @@
+package org.ys.services;
+
+import java.awt.Cursor;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.ys.clientservices.IShoppingService;
+import org.ys.commons.Address;
+import org.ys.commons.Category;
+import org.ys.commons.Customer;
+import org.ys.commons.Order;
+import org.ys.commons.OrderLine;
+import org.ys.commons.Payment;
+import org.ys.commons.Product;
+import org.ys.commons.ShoppingCart;
+import org.ys.idao.ICategoryDAO;
+import org.ys.idao.ICustomerDAO;
+import org.ys.idao.IOrderDAO;
+import org.ys.idao.IOrderLineDAO;
+import org.ys.idao.IPaymentDAO;
+import org.ys.idao.IProductDAO;
+import org.ys.idao.IShoppingCartDAO;
+@Service
+@Transactional(propagation=Propagation.REQUIRES_NEW)
+public class ShoppingService implements IShoppingService{
+	
+	@Autowired
+	private ICustomerDAO customerDAO;
+	@Autowired
+	private IShoppingCartDAO shoppingCartDAO;
+	@Autowired
+	private IOrderLineDAO orderLineDAO;
+	@Autowired
+	private IProductDAO productDAO;
+	@Autowired
+	private ICategoryDAO categoryDAO;
+	@Autowired
+	private IOrderDAO orderDAO;
+	@Autowired
+	private IPaymentDAO paymentDAO;
+
+
+	public Product getProduct(long id) {
+		return productDAO.get(id);
+	}
+	
+	public List<Product> getProducts() {
+		return productDAO.getAll();
+
+	}
+
+	public List<Product> getCategoryProducts(Long category_id) {
+		return categoryDAO.get(category_id).getProducts();
+	}
+
+	public Product getProductDetails(Long product_id) {
+		return productDAO.get(product_id);		
+	}
+
+	public List<OrderLine> addToCart(Customer customer, Long product_id) {
+		OrderLine orderLine = new OrderLine(1,getProduct(product_id).getPrice(),getProduct(product_id));
+		orderLineDAO.create(orderLine);
+		customer.getShoppingCart().addOrderLine(orderLine);
+		customerDAO.update(customer);
+		return customer.getShoppingCart().getOrderLines();		
+	}
+
+	public List<OrderLine> updateCartQuantity(Customer customer, OrderLine orderLine, int quantity) {
+		orderLine.setAmount(orderLine.getAmount()/orderLine.getQuantity()*quantity);
+		orderLine.setQuantity(quantity);
+		orderLineDAO.update(orderLine);
+		return customer.getShoppingCart().getOrderLines();		
+	}
+
+	public List<OrderLine> removeFromCart(Customer customer, OrderLine orderLine) {
+		customer.getShoppingCart().getOrderLines().remove(orderLine);
+		shoppingCartDAO.update(customer.getShoppingCart());
+		return customer.getShoppingCart().getOrderLines();	
+	}
+	
+	public List<OrderLine> getCart(Customer customer) {
+		return customer.getShoppingCart().getOrderLines();
+	}
+
+
+	public Order checkout(Address shippingAddress, Customer customer) {
+		Order order = new Order(new Date(), new Date(), shippingAddress, "Ordered", customer, customer.getShoppingCart());
+		orderDAO.create(order);
+		return order;		
+	}
+
+	public Payment payment(Order order, String details) {
+		Payment payment = new Payment(new Date(), order.getTotalAmount(), details, order);
+		paymentDAO.create(payment);
+		return payment;		
+	}
+}
